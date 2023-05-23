06bc84b8b7c1ca10558c0e116c0edcb75f786c07
João Vitor <joao@joaovitor.com>
2023-05-23 13:28:32 -0300

    Foi instalado o pacote method-override. Foi adicionado as funções: addTask, deleteTask, editTask. Foi criado duas novas rotas: GET /edit/:id, POST /edit/:id. Foi criado o arquivo edit.ejs. No arquivo index.ejs foi alterada a tag textarea para um input do tipo text.

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -9,7 +9,56 @@ const getAllTasks = async (req, res) => {
     }
 }
 
+const addTask = async (req, res) => {
+    const {task} = req.body;
+    const tasDoc = new TaskModel({task});
+    
+    try {
+        await tasDoc.save();
+        res.redirect('/')
+    } catch (err) {
+        res.render('index', {err})
+    }
+}
+
+const deleteTask = async (req, res) => {
+    const {id} = req.body;
+
+    try {
+        await TaskModel.findByIdAndDelete(id);
+        res.redirect('/');
+    } catch (err) {
+        res.send(`erro deleteTask: ${err}`);
+    }
+}
+
+const editTask = async (req, res) => {
+    let {id} = req.params;
+    const {task} = req.body;
+
+    try {
+        await TaskModel.updateOne({_id: id}, {task});
+        res.redirect('/');
+    } catch (err) {
+        res.render('edit', {err, body: req.params});
+    }
+}
+
+const loadTask = async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        let doc = await TaskModel.findById(id);
+        res.render('edit', {err: false, body: doc});
+    } catch (err) {
+        res.status(404).send(`erro loadTask: ${err}`);
+    }
+}
+
 export {
     getAllTasks,
-    
+    addTask,
+    deleteTask,
+    editTask,
+    loadTask,
 }
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,9 +1,21 @@
-import { Router } from "express";
-import { getAllTasks } from "../controllers/taskController.js";
+import e, { Router } from "express";
+import { addTask, deleteTask, getAllTasks, editTask, loadTask } from "../controllers/taskController.js";
+import methodOverride from "method-override";
 
 const routes = Router();
 
+routes.use(methodOverride('_method'));
+
 routes.get('/', getAllTasks);
+
+routes.get('/edit/:id', e.urlencoded({extended: true}), loadTask)
+
+routes.post('/edit/:id', e.urlencoded({extended: true}), editTask);
+
+routes.post('/', e.urlencoded({extended: true}), addTask);
+
+routes.delete('/', e.urlencoded({extended:true}), deleteTask)
+
 
 export {
     routes,
