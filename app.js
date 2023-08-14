3c90146d8107956312e8649d4e7dedecde102000
João Vitor <joao@joaovitor.com>
2023-08-14 10:17:37 -0300

    Importing and configuring dotenv.

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ import { routes } from "./routes/taskRoute.js";
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import serveFavicon from "serve-favicon";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = e();
 const port = 9000;
+const db = process.env.MONGODB_URL
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'templates'));
 
-main().
+main(db).
 catch((err) => {
     console.log(`erro com database: ${err}`);
 });
